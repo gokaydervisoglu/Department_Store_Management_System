808fa812519d725ea7540fae7954e2c5fc2f4098
gokaydervisoglu <gokay_d@outlook.com>
2022-11-04 04:27:53 +0300

    Bonus task file created

---
+++ DepartmentStoreManagementSystem/DepartmentStoreManagementSystem/message.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//messages are numbered with unique ID and this message translated text are  stored on en.dat and tr.dat files
+#define main_menu 1
+#define welcome_to_my_program 2
+#define lets_translate_messages 3
+#define current_lang_setting 4
+#define change_language1 5
+#define enter_your_choice1 6
+#define change_language2 7
+#define exit2 8
+#define exit1 9
+#define calculate_bill1 10
+#define add_goods1 11
+#define edit_goods1 12
+#define display_all1 13
+#define search1 14
+#define delete_goods1 15
+#define calculate_bill2 16
+#define add_goods2 17
+#define edit_goods2 18
+#define display_all2 19
+#define search2 20
+#define delete_goods2 21
